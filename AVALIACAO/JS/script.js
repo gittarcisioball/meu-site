36517b2886030c9a7a6031c0a4d468b0a4da2efc
gittarcisioball <tarcisiovieiraalves@gmail.com>
2025-06-10 20:40:13 -0300

    Delete AVALIACAO directory

--- AVALIACAO/JS/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('btnMensagem').addEventListener('click', function() {
-  const mensagem = document.getElementById('mensagem');
-  mensagem.innerText = "Nunca desista dos seus sonhos, pois é lutando por eles que a vida ganha sentido!";
-  mensagem.style.color = "red";
-});
