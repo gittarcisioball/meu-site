af24dcde9250bdba747fd0a360aa275653ed6046
gittarcisioball <tarcisiovieiraalves@gmail.com>
2025-06-09 23:45:58 -0300

    Add files via upload

---
+++ AVALIACAO/JS/script.js
@@ -0,0 +1,5 @@
+document.getElementById('btnMensagem').addEventListener('click', function() {
+  const mensagem = document.getElementById('mensagem');
+  mensagem.innerText = "Nunca desista dos seus sonhos, pois é lutando por eles que a vida ganha sentido!";
+  mensagem.style.color = "red";
+});
